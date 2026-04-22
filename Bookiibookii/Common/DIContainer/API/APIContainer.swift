1abd2245a45e3dc0426a7940440b5cbb3c5c8c6e
jungee <qhrtj07@gmail.com>
2026-04-23 02:19:10 +0900

    feat: APIContainer에 GroupService 주입

--- Bookiibookii/Common/DIContainer/API/APIContainer.swift
+++ Bookiibookii/Common/DIContainer/API/APIContainer.swift
@@ -4,11 +4,13 @@ final class APIContainer: Sendable {
     let auth: AuthService
     let interceptor: AuthInterceptor
     let user: UserService
+    let group: GroupService
 
     init(auth: AuthService = AuthService()) {
         self.auth = auth
         let interceptor = AuthInterceptor(authService: auth)
         self.interceptor = interceptor
         self.user = UserService(interceptor: interceptor)
+        self.group = GroupService(interceptor: interceptor)
     }
 }
