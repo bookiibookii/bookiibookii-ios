a2d9a0f4372b73f7cecfc1bd2f4d2af405fec50e
jungee <qhrtj07@gmail.com>
2026-06-24 02:27:47 +0900

    색상, 폰트 추가

--- Bookiibookii/Common/Extensions/Font+Pretendard.swift
+++ Bookiibookii/Common/Extensions/Font+Pretendard.swift
@@ -2,9 +2,27 @@ import SwiftUI
 
 extension Font {
     /// Pretendard Variable 폰트. 가변 폰트이므로 weight 파라미터로 굵기 지정.
-    /// 시스템 폰트 대체가 필요할 경우 registered name은 "Pretendard Variable"
     static func pretendard(size: CGFloat, weight: Font.Weight = .regular) -> Font {
         Font.custom("Pretendard Variable", size: size).weight(weight)
+    }
+
+    /// MaruBuri 세리프 폰트 (독서카드/라이브러리 본문·인용 등).
+    /// 정적 폰트라 weight별 PostScript 이름을 직접 매핑
+    static func maruburi(size: CGFloat, weight: Font.Weight = .regular) -> Font {
+        let name: String
+        switch weight {
+        case .ultraLight, .thin:
+            name = "MaruBuri-ExtraLight"
+        case .light:
+            name = "MaruBuri-Light"
+        case .medium, .semibold:
+            name = "MaruBuri-SemiBold"
+        case .bold, .heavy, .black:
+            name = "MaruBuri-Bold"
+        default:
+            name = "MaruBuri-Regular"
+        }
+        return Font.custom(name, size: size)
     }
 }
 
