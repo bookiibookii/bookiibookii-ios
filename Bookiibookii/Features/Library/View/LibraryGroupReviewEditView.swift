b35749a6c08a1e61425d33a5fe6929aee25ec0c1
jungee <qhrtj07@gmail.com>
2026-07-16 03:02:32 +0900

    version, build 번호 변경

--- Bookiibookii/Features/Library/View/LibraryGroupReviewEditView.swift
+++ Bookiibookii/Features/Library/View/LibraryGroupReviewEditView.swift
@@ -1,4 +1,5 @@
 import SwiftUI
+import Combine
 
 private enum ReviewEditStarState { case empty, subPale, sub }
 
--- Bookiibookii/Features/Library/View/LibraryGroupReviewView.swift
+++ Bookiibookii/Features/Library/View/LibraryGroupReviewView.swift
@@ -1,4 +1,5 @@
 import SwiftUI
+import Combine
 
 struct LibraryGroupReviewView: View {
     @EnvironmentObject private var container: DIContainer
